d99186904e903a20f3bc3ab7c825b4949430dc1d
ohashi3589 <sh3589_virtue@yahoo.co.jp>
2023-04-03 09:42:02 +0900

    three

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/reset.css') }}">
 </head>
 <title>Document</title>
 
